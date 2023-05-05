057b8869ec6888709c0103bd0dd20b13fd47e88e
briandenicola <brian@briandenicola.com>
2023-05-05 08:41:39 -0500

    Added privatelink service

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -42,6 +42,8 @@ resource "azurerm_subnet" "privatelink-snat" {
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = [ local.pls_subnet_cidir ]
+  private_endpoint_network_policies_enabled = false
+  private_link_service_network_policies_enabled = false
 }
 
 resource "azurerm_subnet" "private-endpoints" {
--- infrastructure/privatelink-service.tf
+++ infrastructure/privatelink-service.tf
@@ -1,1 +1,39 @@
-#TBD
+data "azurerm_lb" "aks_ingress_lb" {
+    depends_on = [ 
+        azapi_resource.flux_config
+     ]
+    name                = "kubernetes-internal"
+    resource_group_name = "${local.resource_name}_k8s_nodes_rg"
+}
+
+resource "azurerm_private_link_service" "this" {
+  name                                        = "${local.aks_name}-privatelink-service"
+  resource_group_name                         = azurerm_resource_group.this.name
+  location                                    = azurerm_resource_group.this.location
+  enable_proxy_protocol                       = false
+  auto_approval_subscription_ids              = [var.core_subscription]
+  visibility_subscription_ids                 = [var.core_subscription]
+  load_balancer_frontend_ip_configuration_ids = [data.azurerm_lb.aks_ingress_lb.frontend_ip_configuration.0.id]
+
+  nat_ip_configuration {
+    name                       = "primary"
+    private_ip_address_version = "IPv4"
+    subnet_id                  = azurerm_subnet.privatelink-snat.id
+    primary                    = true
+  }
+}
+ 
+resource "azurerm_private_endpoint" "aks_ingress_core" {
+  name                = "${local.aks_name}-ingress-core-endpoint"
+  resource_group_name = var.core_private_endpoint_rg_name
+  location            = azurerm_resource_group.this.location
+  subnet_id           = data.azurerm_subnet.core_private-endpoints.id
+  provider            = azurerm.core
+  
+  private_service_connection {
+    name                           ="${local.aks_name}-ingress-core-endpoint"
+    private_connection_resource_id = azurerm_private_link_service.this.id
+    is_manual_connection           = false
+  }
+
+}
