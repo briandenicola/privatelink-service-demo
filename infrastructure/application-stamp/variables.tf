2ebd502d6cd3373a73d1ad5f147a152255f2ab11
Brian Denicola <brian@briandenicola.com>
2024-02-26 09:12:23 -0600

    Prep for new features

--- infrastructure/application-stamp/variables.tf
+++ infrastructure/application-stamp/variables.tf
@@ -3,28 +3,45 @@ variable "region" {
   default     = "southcentralus"
 }
 
-variable "deploy_bastion" {
-  description = "Deploy a bastion host"
-  default     = false
-}
-
 variable "app_name" {
   description = "The root name for this application deployment"
-}
-
-variable "firewall_policy_id" {
-  description = "Azure Resource Id of the firweall policy to be used by the application stamp"
 }
 
 variable tags {
   description = "Tags to be applied to all resources"
 }
 
-variable "vm_sku" {
+variable "firewall_policy_id" {
+  description = "Azure Resource Id of the firweall policy to be used by the application stamp"
+}
+
+variable "node_sku" {
   description = "The value for the VM SKU"
   default     = "Standard_D4ads_v5"
+  validation {
+    condition     = contains(["item1", "item2", "item3"], var.test_variable)
+    error_message = "Valid values for var: test_variable are (item1, item2, item3)."
+  } 
+}
+
+variable "node_count" {
+  description = "The value for the VM SKU"
+  default     = 1
+  validation {
+    condition     = contains(["item1", "item2", "item3"], var.test_variable)
+    error_message = "Valid values for var: test_variable are (item1, item2, item3)."
+  } 
 }
 
 variable "core_subscription" {
   description = "Core Subscription"
+}
+
+variable "app_subscription" {
+  description = "Core Subscription"
+}
+
+variable "deploy_bastion" {
+  description = "Deploy a bastion host"
+  default     = false
 }
