93e34d49f2513d86f16ceb2bad3d208ab1e9299a
briandenicola <brian@briandenicola.com>
2024-02-28 16:01:17 -0600

    Bug fixes

--- infrastructure/application-stamp/cluster.tf
+++ infrastructure/application-stamp/cluster.tf
@@ -122,11 +122,4 @@ resource "azurerm_kubernetes_cluster" "this" {
     keda_enabled = true
   }
 
-  storage_profile {
-    blob_driver_enabled = true
-    disk_driver_enabled = true
-    disk_driver_version = "v2"
-    file_driver_enabled = true
-  }
-
 }
--- infrastructure/core/main.tf
+++ infrastructure/core/main.tf
@@ -8,7 +8,7 @@ locals {
   resource_name        = var.app_name
   rg_name              = "${local.resource_name}_core_rg"
   firewall_name        = "${local.resource_name}-fw"
-  acr_account_name     = "${replace(local.resource_name,"-","")}acr"
+  acr_account_name     = "${replace(local.resource_name, "-", "")}acr"
   vnet_cidr            = cidrsubnet("10.0.0.0/8", 8, random_integer.vnet_cidr.result)
   pe_subnet_cidir      = cidrsubnet(local.vnet_cidr, 8, 1)
   compute_subnet_cidir = cidrsubnet(local.vnet_cidr, 8, 4)
@@ -17,6 +17,7 @@ locals {
 resource "azurerm_resource_group" "this" {
   name     = local.rg_name
   location = local.location
+  provider = azurerm.core
 
   tags = {
     Application = var.tags
