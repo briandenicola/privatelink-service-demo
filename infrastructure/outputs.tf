1dacb906b6e9ab1a7010770f37e33d258a2ed5e6
briandenicola <brian@briandenicola.com>
2024-04-02 12:37:22 -0500

    Added APP_SUBSCRIPTION_ID to creds

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,14 +1,19 @@
-output APP_NAME {
+output "APP_NAME" {
   value     = local.resource_name
   sensitive = false
 }
 
-output AKS_NAME {
+output "AKS_NAME" {
   value     = module.application-stamp.AKS_CLUSTER_NAME
   sensitive = false
 }
 
-output AKS_RESOURCE_GROUP {
+output "AKS_RESOURCE_GROUP" {
   value     = module.application-stamp.AKS_RESOURCE_GROUP
+  sensitive = false
+}
+
+output "APP_SUBSCRIPTION_ID" {
+  value     = var.dev_subscription
   sensitive = false
 }
